a8b815cf6cc8c1c3219191cfd222f75717fe502a
SandhyaNeelamthodi <129931960+SandhyaNeelamthodi@users.noreply.github.com>
2023-04-14 17:48:13 +0530

    Add files via upload

---
+++ Day 2/Area of circle/Area of circle/Program.cs
@@ -0,0 +1,5 @@
+﻿using system
+namespace circlearea
+{
+    class 
+}
---
+++ Day 2/Area of circle1/Area of circle1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_circle1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            double area;
+            Console.WriteLine("Enter the value of radius: ");
+            radius= int.Parse(Console.ReadLine());
+            area=Math.PI *radius *radius;
+            Console.WriteLine("Area of the circle is: " +area);
+
+
+        }
+    }
+}
