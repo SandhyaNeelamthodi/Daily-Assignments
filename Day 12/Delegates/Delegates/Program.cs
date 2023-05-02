7b05e7c367c2812ba0f81f42f5db8d98ed6d9084
SandhyaNeelamthodi <129931960+SandhyaNeelamthodi@users.noreply.github.com>
2023-05-02 17:19:08 +0530

    Add files via upload

---
+++ Day 12/Delegates/Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Add(int i,int j)
+        {
+            Console.WriteLine("Sum :" + (i + j));
+        }
+        static void Sub(int i,int j)
+        {
+            Console.WriteLine("Sub :"+(i-j));
+        }
+        static void Multiply(int i,int j)
+        {
+            Console.WriteLine("Multiply:"+(i*j));
+        }
+        public delegate void reftomethod(int i ,int j);
+        static void Main(string[] args)
+        {
+            /*Add(10, 20);
+            reftomethod r1 = Add;
+            r1(10,20);
+            reftomethod r2=Sub; 
+            r2(10,20);)*/
+            reftomethod multicast = Add;
+            multicast += Sub;
+            multicast += Multiply;
+            multicast(20, 10);
+        }
+    }
+}
---
+++ Day 12/exceptn2/exceptn2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptn2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b ;
+            int[] a= new int[5];
+            try
+            {
+                b=int.Parse(Console.ReadLine());
+                int i = 10 / b;
+                a[10] = 9;
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("DIVIDE BY ZERO ERROR");
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Index out of bound");
+            }
+            finally
+            {
+
+                Console.WriteLine("finally");
+            }
+        }
+    }
+}
