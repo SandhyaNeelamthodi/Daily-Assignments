a94f99a9309521424b4e4c4e4e58a210fbc9b545
SandhyaNeelamthodi <129931960+SandhyaNeelamthodi@users.noreply.github.com>
2023-05-06 17:53:48 +0530

    Add files via upload

---
+++ Day 1/Helloworld/Helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            int a = 2; int b=3;
+           int Sum = 0;
+            Sum = a + b;
+            Console.WriteLine(Sum);
+        }
+    }
+}
