2a8ebfb6d42d75282aab95e35259be1cf818d037
SandhyaNeelamthodi <129931960+SandhyaNeelamthodi@users.noreply.github.com>
2023-05-07 10:51:13 +0530

    Add files via upload

---
+++ Day 21/BasicMath/BasicMathUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicMathUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicMathUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31a05fca-b566-47ff-9cce-c5770b4b6e53")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day 21/BasicMath/BasicMathUnitTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BasicMath;
+
+namespace BasicMathUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+               Maths bm = new Maths();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            Maths bm = new Maths();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            Maths bm = new Maths();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            Maths bm = new Maths();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+        }
+    }
+}
