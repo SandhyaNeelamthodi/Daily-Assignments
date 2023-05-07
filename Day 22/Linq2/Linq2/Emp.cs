0f1069d8054564b0413285d08f56acbf5a7c5403
SandhyaNeelamthodi <129931960+SandhyaNeelamthodi@users.noreply.github.com>
2023-05-07 10:52:54 +0530

    Add files via upload

---
+++ Day 22/Linq2/Linq2/Emp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq2
+{
+    internal class Emp
+    {
+        string _name;
+        string _city;
+        public string Name { get { return _name; } }
+        public string City { get { return _city; } }
+        public Emp() { }
+        public Emp(string name,string city) 
+        { 
+            _name = name;
+            _city = city;
+        }
+    }
+}
---
+++ Day 22/Linq2/Linq2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emp> Emplist = new List<Emp>();
+         Emplist.Add(new Emp("Ram","Delhi"));
+         Emplist.Add(new Emp("Shyam","Delhi"));
+            Emplist.Add(new Emp("Lal","Tvm"));
+            Emplist.Add(new Emp("Amit","Kolkatta"));
+            Emplist.Add(new Emp("Aman","Pattambi"));
+            var fQuery = from Emp Employee in Emplist
+                         orderby Employee.City
+                         group Employee by Employee.City;
+
+            foreach(var f in fQuery)
+            {
+                Console.WriteLine(" Employee with " + f.Key + " City ");
+                foreach(var e in f)
+                { Console.WriteLine("" + e.Name); }
+            }
+            
+            
+        }
+        
+    }
+}
