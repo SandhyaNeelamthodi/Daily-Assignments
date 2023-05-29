a046486b65dc4555eb16c9d53219d12e7c1fc517
SandhyaNeelamthodi <129931960+SandhyaNeelamthodi@users.noreply.github.com>
2023-05-29 09:25:10 +0530

    Add files via upload

---
+++ Day 40/Duplicatewords/Duplicatewords/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Duplicatewords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of sentences");
+            int n= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the sentences");
+            string[] sentences= new string[n];
+            for(int i= 0; i < n; i++)
+            {
+                sentences[i] = Console.ReadLine();
+            }
+            string[] result = RemoveDuplicates(sentences, n);
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        public static string[] RemoveDuplicates(string[] sentences,int n)
+        {
+            for(int i= 0;i < n;i++) 
+            {
+                string sentence= sentences[i];
+                string result = Regex.Replace(sentence, @"\b(\w+)(\s+\1)+\b", "$1", RegexOptions.IgnoreCase);
+                sentences[i] = result;
+            }
+            return sentences;
+
+        }
+    }
+}
---
+++ Day 40/Inputarray/Inputarray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inputarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the size of array");
+            int n=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the elements of array");
+            int[] a = new int[n];
+            for (int i=0; i<n; i++)
+            {
+                a[i]=int.Parse(Console.ReadLine());
+                
+            }
+            foreach (int i in a)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
